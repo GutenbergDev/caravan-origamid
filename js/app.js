cad169a8fb5a3be19427f7b4a404bb9ee3ec8251
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-09-18 13:14:00 -0300

    Adicionado footer e melhorias no accordion

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-const accordionList = document.querySelectorAll('.js-accordion dt');
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
 
-function activeAccordion() {
-  this.classList.toggle('ativo');
-  this.nextElementSibling.classList.toggle('ativo');
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
-
-accordionList.forEach((item) => {
-  item.addEventListener("click", activeAccordion);
-});
+initAccordion();
